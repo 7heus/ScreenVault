f794b0c286d14f10ef72307f7f79ebd7ee1d7aa0
David Mitreiro <davemitreiro@gmail.com>
2024-08-12 09:40:40 +0100

    Basic Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { getPopularMovies } from "../lib/TMDb";
+import HomePage from "./Pages/HomePage";
+import Catalog from "./Pages/Catalog";
+import Details from "./Pages/Details";
+import Add from "./Pages/Add";
+import Update from "./Pages/Update";
+import AboutUs from "./Pages/AboutUs";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Catalog" element={<Catalog />} />
+      <Route path="/Catalog/Details" element={<Details />} />
+      <Route path="/Add" element={<Add />} />
+      <Route path="/Update" element={<Update />} />
+
+      <Route path="/AboutUs" element={<AboutUs />} />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Pages/AboutUs.jsx
@@ -0,0 +1,43 @@
+export default function AboutUs() {
+  return (
+    <div className="">
+      <h2 className="">About Us</h2>
+      <div className="">
+        <div className="">
+          <img
+            src="https://ca.slack-edge.com/T01BAR6KJP4-U07866FMMUP-gc4092cfc557-512"
+            alt="Carlos Carvalho Profile Pic"
+            className=""
+          />
+          <h2 className="">Carlos Carvalho</h2>
+          <p className=""></p>
+          <a href="">GitHub</a>
+        </div>
+        <div className="">
+          <img
+            src="https://ca.slack-edge.com/T01BAR6KJP4-U078P7JPCSY-a9155ad40165-512"
+            alt="Matheus Almeida Profile Pic"
+            className=""
+          />
+          <h2 className="">Matheus Almeida</h2>
+          <p className=""></p>
+          <a href="">GitHub</a>
+        </div>
+        <div className="">
+          <img
+            src="https://ca.slack-edge.com/T01BAR6KJP4-U078E4D0LNS-3a5d983ff9d6-192"
+            alt="David Profile Pic"
+            className=""
+          />
+          <h2 className="">David Mitreiro</h2>
+          <p className=""></p>
+          <a href="https://github.com/davemitreiro/">GitHub</a>
+        </div>
+      </div>
+      <div className="">
+        <h2 className="">Information about the project</h2>
+        <p className=""></p>
+      </div>
+    </div>
+  );
+}
