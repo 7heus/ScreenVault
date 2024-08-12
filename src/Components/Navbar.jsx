a98260cddabf05ab493f9f4ff8083cd10ac714ae
Theus <matheusgoias@hotmail.com>
2024-08-12 15:30:56 +0100

    navbar search

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,18 @@ import menuIcon from "../assets/menu-icon.png";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import textLogo from "../assets/screenvault-text.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (e) => setQuery(e.target.value);
+
   return (
     <nav className="Navbar">
       <img src={menuIcon} alt="menu-icon" className="menuIcon" />
       <h3 className="searchIcon">🔍</h3>
+      <input type="search" value={query} onChange={updateQuery} />
       <img src={textLogo} alt="logo-text" className="textLogo" />
     </nav>
   );
