1d491c0edaa0cb012aa872d32cd9d9231645f8a1
Theus <matheusgoias@hotmail.com>
2024-08-12 17:05:59 +0100

    refactoring

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -3,6 +3,7 @@ import "./Catalog.css";
 import Card from "../Components/Card";
 import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import MovieWrap from "./MovieWrapper";
 
 export default function Catalog({ popular, topRated, nowPlaying, upcoming }) {
   const [currentPopPage, setCurrentPopPage] = useState(0);
@@ -10,7 +11,7 @@ export default function Catalog({ popular, topRated, nowPlaying, upcoming }) {
   const [currentNPPage, setCurrentNPPage] = useState(0);
   const [currentUCPage, setCurrentUCPage] = useState(0);
 
-  const [popularPage, setPopularPage] = useState(popular[0]);
+  const [popularPage, setPopularPage] = useState(popular[currentPopPage]);
   const [topRatedPage, setTopRatedPage] = useState(topRated[currentTRPage]);
   const [nowPlayingPage, setNowPlayingPage] = useState(
     nowPlaying[currentNPPage]
@@ -22,81 +23,15 @@ export default function Catalog({ popular, topRated, nowPlaying, upcoming }) {
       <Navbar />
 
       <div className="page">
-        <div className="movie-wrap">
-          <h4>Popular Now</h4>
-          {/* <div className="buttons">
-            <button onClick={() => {}}>{"<"}</button>
-            <p>Page: {currentPopPage + 1}</p>
-            <button onClick={() => {}}>{">"}</button>
-          </div> */}
+        {popularPage && <MovieWrap h4={"Popular Now"} dat={popularPage} />}
 
-          <div className="movie">
-            {popularPage.data.results.map((movie, index) => {
-              return (
-                <div className="card" key={index}>
-                  <Card movie={movie} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        {topRatedPage && <MovieWrap h4={"Top Rated"} dat={topRatedPage} />}
 
-        <div className="movie-wrap">
-          <h4>Top Rated</h4>
-          {/* <div className="buttons">
-            <button onClick={() => {}}>{"<"}</button>
-            <p>Page: {currentTRPage + 1}</p>
-            <button onClick={() => {}}>{">"}</button>
-          </div> */}
+        {nowPlayingPage && (
+          <MovieWrap h4={"On Theaters"} dat={nowPlayingPage} />
+        )}
 
-          <div className="movie">
-            {topRatedPage.data.results.map((movie, index) => {
-              return (
-                <div className="card" key={index}>
-                  <Card movie={movie} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="movie-wrap">
-          <h4>On Theaters</h4>
-          {/* <div className="buttons">
-            <button onClick={() => {}}>{"<"}</button>
-            <p>Page: {currentNPPage + 1}</p>
-            <button onClick={() => {}}>{">"}</button>
-          </div> */}
-
-          <div className="movie">
-            {nowPlayingPage.data.results.map((movie, index) => {
-              return (
-                <div className="card" key={index}>
-                  <Card movie={movie} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="movie-wrap">
-          <h4>Upcoming</h4>
-          {/* <div className="buttons">
-            <button onClick={() => {}}>{"<"}</button>
-            <p>Page: {currentUCPage + 1}</p>
-            <button onClick={() => {}}>{">"}</button>
-          </div> */}
-
-          <div className="movie">
-            {upcomingPage.data.results.map((movie, index) => {
-              return (
-                <div className="card" key={index}>
-                  <Card movie={movie} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        {upcomingPage && <MovieWrap h4={"Upcoming"} dat={upcomingPage} />}
       </div>
     </>
   );
