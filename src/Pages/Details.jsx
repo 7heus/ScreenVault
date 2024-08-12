53f28d766e959d0862e000e757eeb34f4ad87642
davemitreiro <davemitreiro@gmail.com>
2024-08-12 17:06:48 +0100

    updated details

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getMovieDetails } from "../../lib/TMDb";
+import Navbar from "../Components/Navbar";
 
 export default function Details() {
   const { itemId } = useParams();
@@ -17,26 +18,27 @@ export default function Details() {
   console.log(movieDetails);
 
   return (
-    <div className="Details">
-      <h1>{movieDetails.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`}
-        alt={movieDetails.original_title}
-        width={500}
-      />
-      <p>{movieDetails.overview}</p>
-      <p>
-        Genre: {movieDetails.genres[0].name} & {movieDetails.genres[1].name}
-      </p>
-      <p>Release Date: {movieDetails.release_date}</p>
-      <p>Rating: {movieDetails.vote_average}</p>
-      <p>Origin Country: {movieDetails.origin_country}</p>
-      <p>Original Language: {movieDetails.original_language.toUpperCase()}</p>
-      <p>Runtime: {movieDetails.runtime} min</p>
-      <p>
-        Production: {movieDetails.production_companies[0].name} &{" "}
-        {movieDetails.production_companies[1].name}
-      </p>
-    </div>
+    <>
+      <Navbar />
+
+      <div className="Details">
+        <h1>{movieDetails.title}</h1>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`}
+          alt={movieDetails.original_title}
+          width={500}
+        />
+        <p>{movieDetails.overview}</p>
+        <p>
+          Genre: {movieDetails.genres[0].name} & {movieDetails.genres[1].name}
+        </p>
+        <p>Release Date: {movieDetails.release_date}</p>
+        <p>Rating: {movieDetails.vote_average}</p>
+        <p>Origin Country: {movieDetails.origin_country}</p>
+        <p>Original Language: {movieDetails.original_language.toUpperCase()}</p>
+        <p>Runtime: {movieDetails.runtime} min</p>
+        <p>Production: {movieDetails.production_companies[0].name}</p>
+      </div>
+    </>
   );
 }
