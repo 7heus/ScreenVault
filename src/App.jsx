9de25f49411f3a7d7a46bfe6c4df8b75d14098ec
Theus <matheusgoias@hotmail.com>
2024-08-12 09:41:22 +0100

    New functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { getPopularMovies } from "../lib/TMDb";
+import { getPopularMovies, getMovieDetails } from "../lib/TMDb";
 
 function App() {
   const [count, setCount] = useState(0);
