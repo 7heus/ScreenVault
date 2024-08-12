4b494b32cfa3a489580a8818c5f2774c6330b862
davemitreiro <davemitreiro@gmail.com>
2024-08-12 15:50:07 +0100

    First Details Page

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,3 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getMovieDetails } from "../../lib/TMDb";
+
 export default function Details() {
-  return <div></div>;
+  const { itemId } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    getMovieDetails(itemId).then((data) => {
+      setMovieDetails(data);
+    });
+  }, [itemId]);
+
+  if (!movieDetails) return <p>Loading...</p>;
+
+  return (
+    <div className="Details">
+      <h1>{movieDetails.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`}
+        alt={movieDetails.original_title}
+        width={500}
+      />
+      <p>{movieDetails.overview}</p>
+      <p>Release Date: {movieDetails.release_date}</p>
+      <p>Rating: {movieDetails.vote_average}</p>
+    </div>
+  );
 }
