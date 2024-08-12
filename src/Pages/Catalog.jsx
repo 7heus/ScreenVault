7b083631f891e70696f361ffda20eeb64483e489
Theus <matheusgoias@hotmail.com>
2024-08-12 12:19:43 +0100

    Implemented catalog
    Error: Page wont re-render when clicking > or < buttons

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -1,9 +1,34 @@
-export default function Catalog({
-  popular,
-  topRated,
-  nowPlaying,
-  upcoming,
-  pageFuncs,
-}) {
-  return <div></div>;
+import { useState } from "react";
+import "./Catalog.css";
+import Card from "../Components/Card";
+import { Link } from "react-router-dom";
+
+export default function Catalog({ popular, topRated, nowPlaying, upcoming }) {
+  const [selectedPage, setSelectedPage] = useState(popular[0]);
+  const [currentPage, setCurrentPage] = useState(selectedPage.data.page);
+
+  return (
+    <>
+      <div className="buttons">
+        <button>Popular</button>
+        <button>Top Rated</button>
+        <button>Upcoming</button>
+        <button>Now Playing</button>
+      </div>
+      <div className="render">
+        <div className="buttons">
+          <button onClick={() => selectedPage.pageFunc(-1, setCurrentPage)}>
+            {"<"}
+          </button>
+          <p>Page: {currentPage}</p>
+          <button onClick={() => selectedPage.pageFunc(+1, setCurrentPage)}>
+            {">"}
+          </button>
+        </div>
+        {selectedPage.data.results.map((movie, index) => {
+          return <Card movie={movie} key={index} />;
+        })}
+      </div>
+    </>
+  );
 }
