0fd9fb5acb0714a75a94433fa6ca8a4a2dd611a1
Theus <matheusgoias@hotmail.com>
2024-08-12 17:05:52 +0100

    working search bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,17 +3,30 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import textLogo from "../assets/screenvault-text.png";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const [query, setQuery] = useState("");
+  const nav = useNavigate();
 
   const updateQuery = (e) => setQuery(e.target.value);
+  const sender = (e) => {
+    if (e.key === "Enter") {
+      nav(`/search?q=${query.replace(" ", "_")}`);
+      location.reload();
+    }
+  };
 
   return (
     <nav className="Navbar">
       <img src={menuIcon} alt="menu-icon" className="menuIcon" />
       <h3 className="searchIcon">🔍</h3>
-      <input type="search" value={query} onChange={updateQuery} />
+      <input
+        type="search"
+        value={query}
+        onChange={updateQuery}
+        onKeyDown={sender}
+      />
       <img src={textLogo} alt="logo-text" className="textLogo" />
     </nav>
   );
