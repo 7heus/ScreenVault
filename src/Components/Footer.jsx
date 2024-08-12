641eb305cbd9f734d4f7d30687fac0abcd6ccd68
davemitreiro <davemitreiro@gmail.com>
2024-08-12 10:43:09 +0100

    updated footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import textLogo from "../assets/screenvault-text.png";
+
+export default function Footer() {
+  return (
+    <footer className="">
+      <a
+        className=""
+        href="https://github.com/7heus/ScreenVault"
+        img
+        src={textLogo}
+        alt="text-logo"
+      ></a>
+    </footer>
+  );
+}
