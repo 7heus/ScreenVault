fe6bbd666c6638c709a4201d2ec8085ba28c8efa
Theus <matheusgoias@hotmail.com>
2024-08-12 10:17:34 +0100

    Create Card.jsx

---
+++ src/Components/Card.jsx
@@ -0,0 +1,37 @@
+export default function Card({ movie }) {
+  const convertDate = (date) => {
+    const splitted = date.split("-");
+    return {
+      year: splitted[0],
+      month: splitted[1],
+      day: splitted[2],
+    };
+  };
+
+  const limitedOverview = (overview) => {
+    const cutOverview = overview.split(" ");
+    if (cutOverview.length >= 15) {
+      cutOverview.slice(0, 14);
+      const joined = cutOverview.join(" ");
+      return `${joined}...`;
+    } else return overview;
+  };
+
+  const date = convertDate(movie.release_date);
+  return (
+    <div className="Card">
+      <img
+        src={`https://image.tmdb.org/t/p/w350/${movie.poster_path}`}
+        alt={movie.original_title}
+      />
+      <h2>
+        {movie.title}
+        {movie.adult && "🔞"}
+      </h2>
+      <p>
+        Released in {date.day}/{date.month}/{date.year}
+      </p>
+      <p>{limitedOverview(movie.overview)}</p>
+    </div>
+  );
+}
