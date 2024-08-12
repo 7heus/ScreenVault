bca72f1b6d905d740f74b5c1a1e904dcc4f56f6f
CarlosCwebdev <CarlosCarvalho1995@hotmail.com>
2024-08-12 18:03:14 +0100

    a

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getMovieDetails } from "../../lib/TMDb";
+import "./Details.css";
+import IMDbpic from "../assets/IMDb.png";
 
 export default function Details() {
   const { itemId } = useParams();
@@ -23,8 +25,11 @@ export default function Details() {
         width={500}
       />
       <p>{movieDetails.overview}</p>
+      <br/>
       <p>Release Date: {movieDetails.release_date}</p>
-      <p>Rating: {movieDetails.vote_average}</p>
+      <br/><br/><br/>
+      <a href="https://www.imdb.com/"><img className="IMDpic" src={IMDbpic}/></a>
+      <p className="rating">Rating: {movieDetails.vote_average}</p>
     </div>
   );
 }
