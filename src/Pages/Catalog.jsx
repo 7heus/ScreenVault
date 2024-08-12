c8cb514d8a01bb00f39ba6e679aa1f35f6989391
Theus <matheusgoias@hotmail.com>
2024-08-12 15:31:02 +0100

    catalog list change

--- src/Pages/Catalog.jsx
+++ src/Pages/Catalog.jsx
@@ -2,36 +2,101 @@ import { useState, useEffect } from "react";
 import "./Catalog.css";
 import Card from "../Components/Card";
 import { Link } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 
 export default function Catalog({ popular, topRated, nowPlaying, upcoming }) {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPage, setSelectedPage] = useState(popular[currentPage]);
-  const pageHandler = (int) => {
-    selectedPage.pageFunc(int);
-  };
+  const [currentPopPage, setCurrentPopPage] = useState(0);
+  const [currentTRPage, setCurrentTRPage] = useState(0);
+  const [currentNPPage, setCurrentNPPage] = useState(0);
+  const [currentUCPage, setCurrentUCPage] = useState(0);
 
-  const categoryHandler = (obj) => {
-    setCurrentPage(0);
-    setSelectedPage(obj[currentPage]);
-  };
+  const [popularPage, setPopularPage] = useState(popular[0]);
+  const [topRatedPage, setTopRatedPage] = useState(topRated[currentTRPage]);
+  const [nowPlayingPage, setNowPlayingPage] = useState(
+    nowPlaying[currentNPPage]
+  );
+  const [upcomingPage, setUpcomingPage] = useState(upcoming[currentUCPage]);
 
   return (
     <>
-      <div className="buttons">
-        <button onClick={() => categoryHandler(popular)}>Popular</button>
-        <button onClick={() => categoryHandler(topRated)}>Top Rated</button>
-        <button onClick={() => categoryHandler(upcoming)}>Upcoming</button>
-        <button onClick={() => categoryHandler(nowPlaying)}>Now Playing</button>
-      </div>
-      <div className="render">
-        <div className="buttons">
-          <button onClick={() => pageHandler(-1)}>{"<"}</button>
-          <p>Page: {currentPage + 1}</p>
-          <button onClick={() => pageHandler(+1)}>{">"}</button>
+      <Navbar />
+
+      <div className="page">
+        <div className="movie-wrap">
+          <h4>Popular Now</h4>
+          {/* <div className="buttons">
+            <button onClick={() => {}}>{"<"}</button>
+            <p>Page: {currentPopPage + 1}</p>
+            <button onClick={() => {}}>{">"}</button>
+          </div> */}
+
+          <div className="movie">
+            {popularPage.data.results.map((movie, index) => {
+              return (
+                <div className="card" key={index}>
+                  <Card movie={movie} />
+                </div>
+              );
+            })}
+          </div>
         </div>
-        {selectedPage.data.results.map((movie, index) => {
-          return <Card movie={movie} key={index} />;
-        })}
+
+        <div className="movie-wrap">
+          <h4>Top Rated</h4>
+          {/* <div className="buttons">
+            <button onClick={() => {}}>{"<"}</button>
+            <p>Page: {currentTRPage + 1}</p>
+            <button onClick={() => {}}>{">"}</button>
+          </div> */}
+
+          <div className="movie">
+            {topRatedPage.data.results.map((movie, index) => {
+              return (
+                <div className="card" key={index}>
+                  <Card movie={movie} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="movie-wrap">
+          <h4>On Theaters</h4>
+          {/* <div className="buttons">
+            <button onClick={() => {}}>{"<"}</button>
+            <p>Page: {currentNPPage + 1}</p>
+            <button onClick={() => {}}>{">"}</button>
+          </div> */}
+
+          <div className="movie">
+            {nowPlayingPage.data.results.map((movie, index) => {
+              return (
+                <div className="card" key={index}>
+                  <Card movie={movie} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="movie-wrap">
+          <h4>Upcoming</h4>
+          {/* <div className="buttons">
+            <button onClick={() => {}}>{"<"}</button>
+            <p>Page: {currentUCPage + 1}</p>
+            <button onClick={() => {}}>{">"}</button>
+          </div> */}
+
+          <div className="movie">
+            {upcomingPage.data.results.map((movie, index) => {
+              return (
+                <div className="card" key={index}>
+                  <Card movie={movie} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </>
   );
