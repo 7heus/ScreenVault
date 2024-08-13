35668ad1be2b0fed2af85584c82652135b812873
CarlosCwebdev <CarlosCarvalho1995@hotmail.com>
2024-08-13 10:19:08 +0100

    HomepageSongIntro

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,6 +9,7 @@ import Footer from "../Components/Footer";
 export default function HomePage() {
   return (
     <div className="">
+      <audio src="src\assets\ElevenLabs_2024-08-13T09_10_24_Harrison Gale – The Velvet Voice_ deep, resonant, powerful, smooth, rich, storytelling, narrator_pvc_s50_sb75_t2.mp3" autoPlay/>
       <Link to="/catalog">
         <img src={logo} alt="ScreenVault Logo" className="logo" />
       </Link>
