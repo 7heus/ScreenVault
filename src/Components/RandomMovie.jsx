cf7af9ac356a72f56d2eaa6920c8231ebada2a3a
davemitreiro <davemitreiro@gmail.com>
2024-08-14 09:58:27 +0100

    Random Audio

--- src/Components/RandomMovie.jsx
+++ src/Components/RandomMovie.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
-import "./RandomMovie.css"; // Import the CSS file
+import "./RandomMovie.css";
+import drumRoll from "../assets/mixkit-drum-roll-566.mp3";
 
 export default function RandomMovie({
   popular,
@@ -10,8 +11,8 @@ export default function RandomMovie({
 }) {
   const [randomMovieData, setRandomMovieData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const audioRef = useRef(null);
 
-  // Combine all movies into a single array
   const allMovies = [
     ...popular.flatMap((page) => page.data.results),
     ...topRated.flatMap((page) => page.data.results),
@@ -20,6 +21,10 @@ export default function RandomMovie({
   ];
 
   const randomizeMovie = () => {
+    if (audioRef.current) {
+      audioRef.current.playbackRate = 1.5;
+      audioRef.current.play();
+    }
     setLoading(true);
     const randomIndex = Math.floor(Math.random() * allMovies.length);
     const randomMovie = allMovies[randomIndex];
@@ -39,6 +44,7 @@ export default function RandomMovie({
     <div className="random-movie-page">
       <h1>Don't know what to watch?</h1>
       <button onClick={randomizeMovie}>Pick a Random Movie</button>
+      <audio ref={audioRef} src={drumRoll} />
       {loading && (
         <div className="movie-wheel">
           <div className="movie-wheel-inner">
@@ -59,7 +65,6 @@ export default function RandomMovie({
       )}
       {randomMovieData && !loading && (
         <div>
-          {/* Display random movie details here */}
           <h2>{randomMovieData.data.results[0].title}</h2>
           <Card movie={randomMovieData.data.results[0]} />
         </div>
