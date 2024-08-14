61a0c93187b8517085134e5e006df59e6da84471
davemitreiro <davemitreiro@gmail.com>
2024-08-14 17:28:39 +0100

    a

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,7 +9,10 @@ import Footer from "../Components/Footer";
 export default function HomePage() {
   return (
     <div className="">
-      <audio src="src\assets\ElevenLabs_2024-08-13T09_10_24_Harrison Gale – The Velvet Voice_ deep, resonant, powerful, smooth, rich, storytelling, narrator_pvc_s50_sb75_t2.mp3" autoPlay/>
+      <audio
+        src="src\assets\ElevenLabs_2024-08-13T09_10_24_Harrison Gale – The Velvet Voice_ deep, resonant, powerful, smooth, rich, storytelling, narrator_pvc_s50_sb75_t2.mp3"
+        autoPlay
+      />
       <Link to="/catalog">
         <img src={logo} alt="ScreenVault Logo" className="logo" />
       </Link>
