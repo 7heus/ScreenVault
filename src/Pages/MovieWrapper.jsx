ed09797679367b3579c0c99dfb4348224b06b38b
Theus <matheusgoias@hotmail.com>
2024-08-14 10:59:28 +0100

    Update MovieWrapper.jsx

--- src/Pages/MovieWrapper.jsx
+++ src/Pages/MovieWrapper.jsx
@@ -1,49 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "../Components/Card";
-
 const NUMBER_OF_ITEMS_PER_PAGE = 4;
-
 export default function MovieWrap({ h4, data, getMoreData }) {
   const [currentPage, setCurrentPage] = useState(1);
-
-  const startIndex =
-    currentPage * NUMBER_OF_ITEMS_PER_PAGE - NUMBER_OF_ITEMS_PER_PAGE;
-  const endIndex = currentPage * NUMBER_OF_ITEMS_PER_PAGE;
-
-  const [displayData, setDisplayData] = useState(
-    data.slice(startIndex, endIndex)
-  );
-
-  const handleNextPage = () => {
+  const [displayData, setDisplayData] = useState([]);
+  useEffect(() => {
+    const startIndex =
+      currentPage * NUMBER_OF_ITEMS_PER_PAGE - NUMBER_OF_ITEMS_PER_PAGE;
+    const endIndex = currentPage * NUMBER_OF_ITEMS_PER_PAGE;
+    setDisplayData(data.slice(startIndex, endIndex));
+  }, [data]);
+  const handleNextPage = async () => {
     const newPage = currentPage + 1;
     setCurrentPage(newPage);
-
     // if items in state are enough to display, otherwise fetch more
     if (data.length > newPage * NUMBER_OF_ITEMS_PER_PAGE) {
       const startIndex =
         newPage * NUMBER_OF_ITEMS_PER_PAGE - NUMBER_OF_ITEMS_PER_PAGE;
       const endIndex = newPage * NUMBER_OF_ITEMS_PER_PAGE;
-
       setDisplayData(data.slice(startIndex, endIndex));
     } else {
-      getMoreData(newPage);
+      await getMoreData(newPage);
     }
   };
-
   const handlePrevPage = () => {
     if (currentPage === 1) return;
-
     const newPage = currentPage - 1;
-
     setCurrentPage(newPage);
-
     const startIndex =
       newPage * NUMBER_OF_ITEMS_PER_PAGE - NUMBER_OF_ITEMS_PER_PAGE;
     const endIndex = newPage * NUMBER_OF_ITEMS_PER_PAGE;
-
     setDisplayData(data.slice(startIndex, endIndex));
   };
-
   return (
     <div className="movie-wrap">
       <h4>{h4}</h4>
@@ -52,7 +40,6 @@ export default function MovieWrap({ h4, data, getMoreData }) {
         <p>Page: {currentPage}</p>
         <button onClick={handleNextPage}>{">"}</button>
       </div>
-
       <div className="movie">
         {displayData.map((movie, index) => {
           return (
