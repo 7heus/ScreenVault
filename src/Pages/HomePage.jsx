63bd4920f507b4d64bd3724f45f39fb38445a346
Theus <matheusgoias@hotmail.com>
2024-08-14 17:26:07 +0100

    backshadow

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,14 +2,13 @@ import logo from "../assets/screenvault-logo.png";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
 
-//Components
-import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
-
 export default function HomePage() {
   return (
     <div className="">
-      <audio src="src\assets\ElevenLabs_2024-08-13T09_10_24_Harrison Gale – The Velvet Voice_ deep, resonant, powerful, smooth, rich, storytelling, narrator_pvc_s50_sb75_t2.mp3" autoPlay/>
+      <audio
+        src="src\assets\ElevenLabs_2024-08-13T09_10_24_Harrison Gale – The Velvet Voice_ deep, resonant, powerful, smooth, rich, storytelling, narrator_pvc_s50_sb75_t2.mp3"
+        autoPlay
+      />
       <Link to="/catalog">
         <img src={logo} alt="ScreenVault Logo" className="logo" />
       </Link>
